dc997a1524ab3fecb083f97be3c197b849c2611b
Luka Burian <lukaburian@gmail.com>
2022-10-05 17:06:01 +0200

    fin jour 1
    affichage de données
     - string interpolation = doubles accolades  {{ }}
    réagir aux evenements
     - (click)="onClickButton()"
    prop personnalisées
     - @Input()
    conditionner l'affichage
     - *ngIf
    afficher des listes
     - *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
+  faceSnaps!: FaceSnap[];
+  mySnap!: FaceSnap;
+  flash!:FaceSnap;
+
+  ngOnInit() {
+    this.faceSnaps = [
+    new FaceSnap(
+      'Shrek',
+      'le seul et l unique',
+      'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/04/Shrek_Swamp_Meme.jpg',
+      new Date,
+      0, 
+      'swamp'
+    ),
+
+    new FaceSnap(
+      'Flash McQueen',
+      'le fast',
+      'https://i.pinimg.com/originals/c8/6c/86/c86c8624c53c0024a88f8a8d5ac99888.jpg',
+      new Date,
+      213
+    )];
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,28 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
 
+  @Input() faceSnap!: FaceSnap;
+  buttonString!: string;
+
+  ngOnInit(){
+    this.buttonString = 'hello';
+  }
+
+  onSnap(){
+    if(this.buttonString=='hello'){
+      this.buttonString = 'bye';
+      this.faceSnap.snaps++;
+    }
+    else{
+      this.buttonString = 'hello';
+      this.faceSnap.snaps--;
+    }
+  }
 }
