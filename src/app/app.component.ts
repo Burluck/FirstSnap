35f870576507f4d27e231a41819b9146f8be4705
Luka Burian <lukaburian@gmail.com>
2022-10-07 16:39:08 +0200

    Fin jour 3
    Debut tuto 2
    Découverte observateur et les opérations
    formulaire réactif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concatMap, delay, exhaustMap, filter, interval, map, mergeMap, Observable, of, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-    ngOnInit() {
+  
+  ngOnInit(): void {
+    
   }
+  /*
+  interval$!: Observable<string>;
+    ngOnInit() {
+      this.interval$ = interval(1000).pipe(
+        filter(value => value % 3 === 0),
+        map(value => value % 2 === 0 ?
+          `${value} chiffre pair`:
+          `${value} chiffre impair`),
+        tap(text => this.logger(text))
+      );
+  }
+
+  logger(text: string){
+    console.log('log:' + text);
+  }
+  
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
+
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }*/
 }
